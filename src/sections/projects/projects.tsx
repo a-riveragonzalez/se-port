bc7b90b5337c5dc04dfa9cdbabac127c50ff0590
Arely Rivera <alltherelys@Arelys-MacBook-Pro.local>
2025-03-20 16:24:00 -0700

    added backgrounds

--- src/sections/projects/projects.tsx
+++ src/sections/projects/projects.tsx
@@ -1,93 +1,154 @@
-import * as React from 'react';
-import Typography from '@mui/material/Typography';
-import { Container, Grid2 } from '@mui/material';
-import ProjectCard from './project-card';
+"use client";
 
-import tattooImage1 from '../../assets/images/projects/tattoo_artist_1.png'
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+import {
+  Box,
+  Container,
+  Grid2,
+  Theme,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import ProjectCard from "./project-card";
+
+import tattooImage1 from "../../assets/images/projects/tattoo_artist_1.png";
 
 const projects = [
   {
     id: 1,
     icon: tattooImage1.src,
     title: "Tattoo Shop",
-    websiteLink: '', 
-    githubLink: '',
-    description: "Leveraging the power of TypeScript, JavaScript, and modern frameworks to build robust web applications."
+    websiteLink: "",
+    githubLink: "",
+    description:
+      "Leveraging the power of TypeScript, JavaScript, and modern frameworks to build robust web applications.",
   },
   {
     id: 2,
     icon: tattooImage1.src,
     title: "Tattoo Shop",
-    websiteLink: '', 
-    githubLink: '',
-    description: "Creating dynamic, responsive user interfaces with React and Next.js."
+    websiteLink: "",
+    githubLink: "",
+    description:
+      "Creating dynamic, responsive user interfaces with React and Next.js.",
   },
   {
     id: 3,
     icon: tattooImage1.src,
     title: "Tattoo Shop",
-    websiteLink: '', 
-    githubLink: '',
-    description: "Designing and implementing efficient RESTful and GraphQL APIs with Node.js."
+    websiteLink: "",
+    githubLink: "",
+    description:
+      "Designing and implementing efficient RESTful and GraphQL APIs with Node.js.",
   },
   {
     id: 4,
     icon: tattooImage1.src,
     title: "Tattoo Shop",
-    websiteLink: '', 
-    githubLink: '',
-    description: "Utilizing AWS services and PostgreSQL to develop scalable, data-driven solutions."
-  }
+    websiteLink: "",
+    githubLink: "",
+    description:
+      "Utilizing AWS services and PostgreSQL to develop scalable, data-driven solutions.",
+  },
 ];
 
 export default function Projects() {
+  const theme = useTheme();
+
+  const isMobile = useMediaQuery((theme: Theme) =>
+    theme.breakpoints.down("md")
+  );
+  const isLightMode = theme.palette.mode === "light";
+
   return (
-    <Container
-    sx={{
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-      py: { xs: 5, md: 10 },
-      textAlign: "center",
-    }}
-  >
-    <Typography
-      variant="h6"
-      component="h2"
+    <Box
       sx={{
-        color: "secondary.main",
+        position: "relative",
+        overflow: "hidden",
       }}
     >
-      --- My Projects ---
-    </Typography>
-    <Typography
-      variant="h4"
-      component="h3"
-      sx={{
-        color: "text.primary",
-        fontWeight: 800,
-      }}
-    >
-      Crafting Solutions: 
-      <br />
-      My Work in Action
-    </Typography>
+      {/* Background color layer */}
+      <Box
+        sx={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundColor: isLightMode ? "#fdfdfd" : "#1D1419",
+          opacity: isLightMode ? 0.7 : 0.5,
+          zIndex: 1,
+        }}
+      />
 
-    <Grid2 container spacing={3} sx={{ mt: 2 }}>
-        {projects.map((project) => (
-          <Grid2 size={{xs: 12, sm: 6, md: 3}} key={project.id}>
-            <ProjectCard
-              icon={project.icon}
-              title={project.title}
-              description={project.description}
-              websiteLink={project.websiteLink}
-              githubLink={project.githubLink}
-            />
-          </Grid2>
-        ))}
-      </Grid2>
+      {/* Background image layer with 30% opacity */}
+      <Box
+        sx={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundImage: isMobile
+            ? "url(/assets/backgrounds/mobile/project-bg-m.png)"
+            : "url(/assets/backgrounds/desktop/project-bg-d.png)",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          opacity: 0.6,
+          zIndex: 0,
+        }}
+      />
 
-  </Container>
+      <Container
+        sx={{
+          position: "relative",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          py: { xs: 5, md: 10 },
+          textAlign: "center",
+          zIndex: 2,
+          minHeight: "400px",
+        }}
+      >
+        <Typography
+          variant="h6"
+          component="h2"
+          sx={{
+            color: "secondary.main",
+          }}
+        >
+          --- My Projects ---
+        </Typography>
+        <Typography
+          variant="h4"
+          component="h3"
+          sx={{
+            color: "text.primary",
+            fontWeight: 800,
+          }}
+        >
+          Crafting Solutions:
+          <br />
+          My Work in Action
+        </Typography>
+
+        <Grid2 container spacing={3} sx={{ mt: 2 }}>
+          {projects.map((project) => (
+            <Grid2 size={{ xs: 12, sm: 6, md: 3 }} key={project.id}>
+              <ProjectCard
+                icon={project.icon}
+                title={project.title}
+                description={project.description}
+                websiteLink={project.websiteLink}
+                githubLink={project.githubLink}
+              />
+            </Grid2>
+          ))}
+        </Grid2>
+      </Container>
+    </Box>
   );
 }
